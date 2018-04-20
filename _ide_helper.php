fe6142de3db6a35fce06f990083217e7beda3dfd
kashish <ka399@njit.edu>
2018-04-20 19:45:44 -0400

    Heroku Postgress Seed Issue : Moved "fzaninotto/faker": "^1.4" from require-dev to require in composer.json file.

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.17 on 2018-04-19 15:51:18.
+ * Generated for Laravel 5.6.17 on 2018-04-20 23:43:07.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
