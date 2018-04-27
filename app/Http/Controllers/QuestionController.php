67d06ed0a950ed235a99e31db5f7591e21271bd2
kashish <ka399@njit.edu>
2018-04-27 03:00:26 -0400

    QuestionUpdate : Added Code (question,questionform View, QuestionController, Route) in order to edit and update existing question and display the relevant message.

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -90,14 +90,15 @@ class QuestionController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified question.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Question $question)
     {
-        //
+        $edit = TRUE;
+        return view('questionForm', ['question' => $question, 'edit' => $edit ]);
     }
 
     /**
@@ -107,9 +108,17 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Question $question)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $question->body = $request->body;
+        $question->save();
+        return redirect()->route('question.show',['question_id' => $question->id])->with('message', 'Saved');
     }
 
     /**
--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -13,14 +13,11 @@
                     </div>
                     <div class="card-footer">
                         <a class="btn btn-primary float-right"
-                           href="{{ route('questions.edit',['id'=> $question->id])}}">
+                           href="{{ route('question.edit',['id'=> $question->id])}}">
                             Edit Question
                         </a>
 
-                        {{ Form::open(['method'  => 'DELETE', 'route' => ['questions.destroy', $question->id]])}}
-                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
-                        </button>
-                        {!! Form::close() !!}
+
                     </div>
                 </div>
             </div>
@@ -28,7 +25,7 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header"><a class="btn btn-primary float-left"
-                                                href="{{ route('answers.create', ['question_id'=> $question->id])}}">
+                                                href="">
                             Answer Question
                         </a></div>
 
